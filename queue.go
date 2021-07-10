3c78796e6fa22782cb26cbf165a80a978f30adb5
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-07-10 14:14:55 +0700

    feat(queue): func to create options

--- queue.go
+++ queue.go
@@ -11,6 +11,40 @@ type MQConfigQueue struct {
 	Args             amqp.Table
 }
 
+func NewQueueOptions() *MQConfigQueue {
+	return &MQConfigQueue{}
+}
+
+func (config *MQConfigQueue) SetName(Name string) *MQConfigQueue {
+	config.Name = Name
+	return config
+}
+
+func (config *MQConfigQueue) SetDurable(Durable bool) *MQConfigQueue {
+	config.Durable = Durable
+	return config
+}
+
+func (config *MQConfigQueue) SetDeleteWhenUnused(DeleteWhenUnused bool) *MQConfigQueue {
+	config.DeleteWhenUnused = DeleteWhenUnused
+	return config
+}
+
+func (config *MQConfigQueue) SetExclusive(Exclusive bool) *MQConfigQueue {
+	config.Exclusive = Exclusive
+	return config
+}
+
+func (config *MQConfigQueue) SetNoWait(NoWait bool) *MQConfigQueue {
+	config.NoWait = NoWait
+	return config
+}
+
+func (config *MQConfigQueue) SetArgs(Args amqp.Table) *MQConfigQueue {
+	config.Args = Args
+	return config
+}
+
 func NewQueue(channel *amqp.Channel, queue *MQConfigQueue) (amqp.Queue, error) {
 	return channel.QueueDeclare(
 		queue.Name,
