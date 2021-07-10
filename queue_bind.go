721c27149b30404c518b85c7359859297187936f
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-07-10 14:24:34 +0700

    feat(queue-bind): func to create options

--- queue_bind.go
+++ queue_bind.go
@@ -10,6 +10,35 @@ type MQConfigQueueBind struct {
 	Args       amqp.Table
 }
 
+func NewQueueBindOptions() *MQConfigQueueBind {
+	return &MQConfigQueueBind{}
+}
+
+func (config *MQConfigQueueBind) SetName(Name string) *MQConfigQueueBind {
+	config.Name = Name
+	return config
+}
+
+func (config *MQConfigQueueBind) SetRoutingKey(RoutingKey string) *MQConfigQueueBind {
+	config.RoutingKey = RoutingKey
+	return config
+}
+
+func (config *MQConfigQueueBind) SetExchange(Exchange string) *MQConfigQueueBind {
+	config.Exchange = Exchange
+	return config
+}
+
+func (config *MQConfigQueueBind) SetNoWait(NoWait bool) *MQConfigQueueBind {
+	config.NoWait = NoWait
+	return config
+}
+
+func (config *MQConfigQueueBind) SetArgs(Args amqp.Table) *MQConfigQueueBind {
+	config.Args = Args
+	return config
+}
+
 func NewQueueBind(channel *amqp.Channel, bind *MQConfigQueueBind) error {
 	return channel.QueueBind(
 		bind.Name,
