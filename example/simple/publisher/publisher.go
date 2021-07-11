17582ff02f57a7a2e46a3400d95521a8ff4d5cce
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-07-11 10:43:59 +0700

    feat(example): simple publisher

---
+++ example/simple/publisher/publisher.go
@@ -0,0 +1,50 @@
+package main
+
+// this is an example of publising message directly into queue
+// steps to do here are same as https://www.rabbitmq.com/tutorials/tutorial-one-go.html
+// but using the wrapper API
+
+import (
+	"fmt"
+	"log"
+	"sync"
+
+	rabbitmq "github.com/hadihammurabi/go-rabbitmq"
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	mq, err := rabbitmq.NewMQ("amqp://guest:guest@localhost:5672/")
+	failOnError(err, fmt.Sprintf("%v", err))
+	defer func() {
+		mq.GetConnection().Close()
+		mq.GetChannel().Close()
+	}()
+
+	_, err = mq.DeclareQueue(rabbitmq.NewQueueOptions().SetName("hello"))
+	failOnError(err, fmt.Sprintf("%v", err))
+
+	var wg sync.WaitGroup
+	max := 10
+	wg.Add(max)
+	for i := 0; i < max; i++ {
+		go func(a int) {
+			defer wg.Done()
+			body := fmt.Sprintf("Hello World %d !", a)
+			err = mq.Publish(&rabbitmq.MQConfigPublish{
+				RoutingKey: mq.GetQueue().Name,
+				Message: amqp.Publishing{
+					ContentType: "text/plain",
+					Body:        []byte(body),
+				},
+			})
+		}(i)
+	}
+	wg.Wait()
+}
